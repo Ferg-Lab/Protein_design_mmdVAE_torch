865aa3cbd979f9347ddf3d56056eb60dd6b561f2
chemgeeklian <xinran.lian@gmail.com>
2023-01-06 15:57:32 -0600

    bug fix

--- source/preprocess.py
+++ source/preprocess.py
@@ -44,7 +44,28 @@ def OneHot_encode(data,sample):
     return b
 
 
+def create_one_hot(df):
+    """
+    function description: create one hot encoded tensors
+    
+    """
+    # We will use this dictionary to map each character to an integer so that it can be used as an input to our ML models:
+    dict_int2aa = {0:"A",1:"C",2:"D",3:"E",4:"F",5:"G",6:"H",7:"I",8:"K",9:"L",10:"M",11:"N",12:"P",13:"Q",14:"R",15:"S",16:"T",17:"V",18:"W",19:"Y",20:"-"}
 
+    token2int = {x:i for i, x in enumerate('ACDEFGHIKLMNPQRSTVWY-')}
+
+    train_inputs = preprocess_inputs(df, token2int)
+    
+    sample = len(df)
+    positions = len(df.iloc[0, 0])
+    
+    hot_inputs = np.zeros((train_inputs.shape[0], positions, 21))
+    
+    for sample in range(train_inputs.shape[0]):
+        hot_encoded = OneHot_encode(train_inputs, sample)
+        hot_inputs[sample, :,:] = hot_encoded
+        
+    return hot_inputs
 
 
 
